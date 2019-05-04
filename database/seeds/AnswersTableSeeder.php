910d719b5158df0d448d379ce00fceda5d1b0338
unuoma <umo2@njit.edu>
2019-05-04 00:41:03 -0400

    Added profile (question and answer included)

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -12,7 +12,7 @@ class AnswersTableSeeder extends Seeder
     public function run()
     {
         $users = App\User::inRandomOrder();
-       // for ($i = 1; $i <= 6; $i++) {
+       for ($i = 1; $i <= 6; $i++) {
             $users->each(function ($user) {
                 $question = App\Question::inRandomOrder()->first();
                 $answer = factory(\App\Answer::class)->make();
@@ -20,6 +20,6 @@ class AnswersTableSeeder extends Seeder
                 $answer->question()->associate($question);
                 $answer->save();
             });
-        //}
+        }
     }
 }
