17cc94b2a2dce9a7798a50d323ee74ebc98e3bf3
unuoma <umo2@njit.edu>
2019-05-05 14:43:01 -0400

    Added Create profile form.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -24,7 +25,9 @@ class ProfileController extends Controller
      */
     public function create()
     {
-        //
+        $profile = new Profile();
+        $edit = FALSE;
+        return view('profileForm', ['profile' => $profile,'edit' => $edit  ]);
     }
 
     /**
@@ -35,7 +38,24 @@ class ProfileController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+            'body' => 'required',
+        ], [
+
+            'fname.required' => ' First is required',
+            'lname.required' => ' Last is required',
+            'body.required' => ' Body is required',
+        ]);
+        $input = request()->all();
+
+        $profile = new Profile($input);
+        $profile->user()->associate(Auth::user());
+        $profile->save();
+
+        return redirect()->route('home')->with('message', 'Profile Created');
+
     }
 
     /**
